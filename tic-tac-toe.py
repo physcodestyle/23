518f23a74694c5ba00f6265c698900b3b0f3e9e9
Roman Popov <pri20051982@gmail.com>
2024-03-22 01:19:47 +0300

    - Оформление кода было отредактировано в некоторых местах в соответствии с PEP 8 - Названия нескольких функций были изменены для более лёгкого восприятия - Реализована проверка завершения игры через "дерево" (подход был изменён и оптимизирован) - Исправление багов (таких как: смещение координаты y при её передаче от пользователя, неправильный возврат координат (не как пара, а как отдельные аргументы - были пропущены скобки), отсутствие возвращаемого значения при его необходимости и т.д)

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,6 +12,7 @@ messages = {
     'WRONG_COORDS': 'Выбранное Вами поле занято, введите корректные координаты...',
 }
 
+
 def get_field_default_state(col_count, row_count):
     field = []
     for i in range(row_count):
@@ -21,8 +22,10 @@ def get_field_default_state(col_count, row_count):
         field.append(row)
     return field
 
+
 def get_square_field_default_state(size):
     return get_field_default_state(size, size)
+
 
 def get_cell_view(value):
     value_sign = ' '
@@ -32,11 +35,13 @@ def get_cell_view(value):
         value_sign = 'O'
     return ' ' + value_sign + ' '
 
+
 def get_delimiter_cell_view(symbol_count):
     str = ''
     for i in range(symbol_count):
         str += '-'
     return str
+
 
 def print_field(field):
     col_count = len(field[0])
@@ -51,13 +56,15 @@ def print_field(field):
         if i < len(field) - 1:
             print(delimiter)
 
+
 def create_list(size, element):
     list = []
     for _ in range(size):
         list.append(element)
     return list
 
-def process_field(field, callback, params = None):
+
+def process_field(field, callback, params=None):
     row_count = len(field)
     col_count = len(field[0])
     for i in range(row_count):
@@ -66,12 +73,15 @@ def process_field(field, callback, params = None):
                 return True
     return False
 
+
 def get_matrix_field_processing_callback(field, coords, params):
+    # todo fix
     value = field[coords[0]][coords[1]]
     matrix_coords = (params['limit'] - 1 + coords[0] - params['last_coords'][0], params['limit'] - 1 + coords[1] - params['last_coords'][1])
     if matrix_coords[0] >= 0 and matrix_coords[1] >= 0 and matrix_coords[0] < params['limit'] * 2 - 1 and matrix_coords[1] < params['limit'] * 2 - 1:
         params['matrix'][matrix_coords[0]][matrix_coords[1]] = value
     return False
+
 
 def get_string_from_matrix_by_pattern(matrix, pattern):
     sequence = []
@@ -79,47 +89,85 @@ def get_string_from_matrix_by_pattern(matrix, pattern):
         sequence.append(matrix[coords[0]][coords[1]])
     output_string = ""
     for item in sequence:
-        if item == states["u_cell"]:
-            output_string += "U"
-        elif item == states["x_cell"]:
-            output_string += "X"
-        elif item == states["o_cell"]:
-            output_string += "O"
+        output_string += transform_item_to_string(item)
+    return output_string
+
 
 def get_size(limit):
     return limit * 2 - 1
 
-def get_patterns(limit):
+
+def get_patterns_for_matrix(limit):
     patterns_list = [[], [], [], []]
     size = get_size(limit)
     for i in range(size):
-        patterns_list[0].append((i, i))         # back slash
-        patterns_list[1].append((i, limit))     # vertical
-        patterns_list[2].append((i, size - i))  # slash
-        patterns_list[3].append((limit, i))     # horizontal
+        patterns_list[0].append((i, i))             # back slash
+        patterns_list[1].append((i, limit - 1))     # vertical
+        patterns_list[2].append((i, size - 1 - i))  # slash
+        patterns_list[3].append((limit - 1, i))     # horizontal
+    return patterns_list
 
-def is_not_finishing_by_matrix(field, last_coords, limit):
+
+def is_finishing_by_matrix(field, last_coords, limit):
     size = get_size(limit)
     row = create_list(size, states['u_cell'])
     matrix = create_list(size, row.copy())
-    process_field(field, get_matrix_field_processing_callback, { "matrix": matrix, "last_coords": last_coords, "limit": limit })
-    for p in get_patterns(limit):
+    process_field(field, get_matrix_field_processing_callback,
+                  {"matrix": matrix, "last_coords": last_coords, "limit": limit})
+    for p in get_patterns_for_matrix(limit):
         string_by_pattern = ''.join(get_string_from_matrix_by_pattern(matrix, p))
         if 'X' * limit in string_by_pattern or 'O' * limit in string_by_pattern:
             return True
     return False
 
-def is_not_finishing_by_tree(field, last_coords, limit):
-    pass
 
-def is_field_full(field):
-    return process_field(field, (lambda f, indices, _ : f[indices[0]][indices[1]] == states['u_cell']))
+def get_value_from_field_safe(field, coords):
+    try:
+        return field[coords[0]][coords[1]]
+    except IndexError:
+        return states['u_cell']
+
+
+def transform_item_to_string(item):
+    if item == states["o_cell"]:
+        return "O"
+    elif item == states["x_cell"]:
+        return "X"
+    else:
+        return "U"
+
+
+def get_strings_for_tree(field, last_coords, limit):
+    strings_set = ["", "", "", ""]
+    length = get_size(limit)
+    coords_diff = last_coords[0] - last_coords[1]
+    loop_diff = int((length - 1)/2)
+    for i in range(-1 * loop_diff, loop_diff + 1):
+        strings_set[0] += transform_item_to_string(get_value_from_field_safe(field, (last_coords[0] + i, last_coords[0] + i - coords_diff)))
+        strings_set[1] += transform_item_to_string(get_value_from_field_safe(field, (last_coords[0] + i, last_coords[1])))
+        strings_set[2] += transform_item_to_string(get_value_from_field_safe(field, (-1 * (last_coords[0] + i), last_coords[0] + i - coords_diff)))
+        strings_set[3] += transform_item_to_string(get_value_from_field_safe(field, (last_coords[0], last_coords[0] + i - coords_diff)))
+    return strings_set
+
+
+def is_finishing_by_tree(field, last_coords, limit):
+    for s in get_strings_for_tree(field, last_coords, limit):
+        if 'X' * limit in s or 'O' * limit in s:
+            return True
+    return False
+
+
+def is_field_not_full(field):
+    return process_field(field, (lambda f, indices, _: f[indices[0]][indices[1]] == states['u_cell']))
+
 
 def is_game_not_finished(field, last_coords):
-    if (is_field_full(field) if is_finite_field else True):
-        return is_not_finishing_by_matrix(field, last_coords, sign_sequence_limit)
+    if (is_field_not_full(field) if is_finite_field else True):
+        # return not is_finishing_by_matrix(field, last_coords, sign_sequence_limit)
+        return not is_finishing_by_tree(field, last_coords, sign_sequence_limit)
     else:
         return False
+
 
 def put_value(field, row_index, col_index, new_value):
     if field[row_index][col_index] == states['u_cell']:
@@ -127,21 +175,25 @@ def put_value(field, row_index, col_index, new_value):
         return True
     return False
 
+
 def put_player_input(field, value):
     input_string = input(messages['ENTER_COORDS'])
     coords = input_string.split(',')
     if put_value(field, int(coords[0]) - 1, int(coords[1]) - 1, value):
-        return (True, int(coords[0]) - 1, int(coords[1]))
+        return True, (int(coords[0]) - 1, int(coords[1]) - 1)
     else:
         print(messages['WRONG_COORDS'])
-        return (False, int(coords[0]) - 1, int(coords[1]))
+        return False, (int(coords[0]) - 1, int(coords[1]) - 1)
+
 
 def put_computer_value(field, value):
-    coords = (2,2)
-    return (put_value(field, int(coords[0]) - 1, int(coords[1]) - 1, value), (int(coords[0]) - 1, int(coords[1])))
+    coords = (2, 2)
+    return (put_value(field, int(coords[0]) - 1, int(coords[1]) - 1, value), (int(coords[0]) - 1, int(coords[1]) - 1))
+
 
 def init(field_size):
     return get_square_field_default_state(field_size)
+
 
 def play(field):
     current_value = states['x_cell']
@@ -156,10 +208,11 @@ def play(field):
             last_coords = result[1]
         else:
             result = put_player_input(field, current_value)
-            while(result[0] == False):
+            while (result[0] == False):
                 result = put_player_input(field, current_value)
             last_coords = result[1]
 
         current_value = states['x_cell'] if current_value == states['o_cell'] else states['o_cell']
+
 
 play(init(3))
