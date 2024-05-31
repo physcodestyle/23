15fa6fc36ee37bb6c23e111099b285e7c3385250
Igor Korovchenko <igsekor@gmail.com>
2024-05-31 21:24:41 +0300

    Дорабатывает клиентскую часть

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const serverHost = 'http://127.0.0.1:8000'
 
-const field = {
-  columns: 7,
-  count: 35,
-  map: {
-    '17': 'cross',
-    '9': 'oval'
+let field = {
+  columns: 0,
+  count: 0,
+  map: {}
+}
+
+function buildFieldObject(fieldMatrix) {
+  const cross = []
+  const oval = []
+  let columns = fieldMatrix[0].length
+  let count = 0
+  for (let i = 0; i < fieldMatrix.length; i++) {
+    for (let j = 0; j < fieldMatrix[i].length; j++) {
+      count++
+      if (fieldMatrix[i][j] === null) {
+      } else if (fieldMatrix[i][j]) {
+        cross.push(columns * i + j)
+      } else {
+        oval.push(columns * i + j)
+      }
+    }
+  }
+  const map = []
+  const preparedCross = cross.reduce((r, c) => { return {...r, [c]: 'cross'} }, {})
+  const preparedOval = oval.reduce((r, o) => { return {...r, [o]: 'oval'} }, {})
+  Object.keys(preparedCross).forEach((k) => map[k] = preparedCross[k])
+  Object.keys(preparedOval).forEach((k) => map[k] = preparedOval[k])
+  return {
+    columns,
+    count,
+    map,
   }
 }
 
@@ -24,11 +49,10 @@ function serializeForm(formNode) {
   return data
 }
 
-async function sendData(x, y, field) {
+async function sendData(x, y) {
   return await fetch(`${serverHost}/cross/${x}/${y}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: 'field',
   })
     .then(async (r) => {
       if (r.status === 200) {
@@ -36,13 +60,18 @@ async function sendData(x, y, field) {
       } else {
         onError(await r.json())
       }
-      
     })
 }
 
 function onSuccess(data) {
   console.log(data)
-  console.log('Ваш ход отправлен!')
+  if (data['turn_result'][0]) {
+    field = buildFieldObject(data['turn_result'][1])
+    if (data['turn_result'][2]) {
+      console.log(data['turn_result'][3])
+    }
+  }
+  setupField()
 }
 
 function onError(data) {
@@ -50,45 +79,62 @@ function onError(data) {
   console.error('Не удалось отправить ход!')
 }
 
-const checkedCells = Object.keys(field.map).map((k) => Number(k))
+function setupField() {
+  const checkedCells = Object.keys(field.map).map((k) => Number(k))
 
-const form = document.querySelector('.form')
-form.addEventListener('submit', async (event) => {
-  event.preventDefault()
-  const turnData = event.target.getAttribute('data-turn')
-  const data = serializeForm(event.target)
-    .filter((c) => c.value === true)
-    .filter((c) => c.name === turnData)
-  await sendData(3, 5, data)
-  document.querySelector(`input[name='${turnData}']`).disabled = true
-})
-
-const map = document.querySelector('.field')
-map.setAttribute('style', `grid-template-columns: repeat(${field.columns}, min-content);`)
-
-for (let i = 0; i < field.count; i++) {
-  const label = document.createElement('label')
-  label.classList.add('sign')
-
-  const row = Math.floor((i + 1) / field.columns)
-  const col = i - row * field.columns
-  const checkbox = document.createElement('input')
-  checkbox.setAttribute('type', 'checkbox')
-  checkbox.setAttribute('name', `cell-${col < 0 ? row - 1 : row}-${col < 0 ? field.columns - 1 : col}`)
-  checkbox.classList.add('cell')
-  if (checkedCells.includes(i)) {
-    checkbox.classList.add(`cell--${field.map[String(i)]}`)
-    checkbox.checked = true
-    checkbox.disabled = true
-  } else {
-    checkbox.classList.add('cell--empty')
-  }
-  checkbox.addEventListener('click', (event) => {
-    event.target.classList.add('cell--cross')
-    form.setAttribute('data-turn', event.target.name)
-    form.dispatchEvent(new Event('submit'))
+  const form = document.querySelector('.form')
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const turnData = event.target.getAttribute('data-turn')
+    const data = serializeForm(event.target)
+      .filter((c) => c.value === true)
+      .filter((c) => c.name === turnData)
+    await sendData(3, 5, data)
+    document.querySelector(`input[name='${turnData}']`).disabled = true
   })
-  
-  label.appendChild(checkbox)
-  map.appendChild(label)
+
+  const map = document.querySelector('.field')
+  while (map.firstChild) {
+    map.removeChild(map.firstChild)
+  }
+  map.setAttribute('style', `grid-template-columns: repeat(${field.columns}, min-content);`)
+
+  for (let i = 0; i < field.count; i++) {
+    const label = document.createElement('label')
+    label.classList.add('sign')
+
+    const row = Math.floor((i + 1) / field.columns)
+    const col = i - row * field.columns
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.setAttribute('name', `cell-${col < 0 ? row - 1 : row}-${col < 0 ? field.columns - 1 : col}`)
+    checkbox.classList.add('cell')
+    if (checkedCells.includes(i)) {
+      checkbox.classList.add(`cell--${field.map[String(i)]}`)
+      checkbox.checked = true
+      checkbox.disabled = true
+    } else {
+      checkbox.classList.add('cell--empty')
+    }
+    checkbox.addEventListener('click', (event) => {
+      event.target.classList.add('cell--cross')
+      form.setAttribute('data-turn', event.target.name)
+      form.dispatchEvent(new Event('submit'))
+    })
+    
+    label.appendChild(checkbox)
+    map.appendChild(label)
+  }
 }
+
+fetch(`${serverHost}/game/start`, {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+})
+  .then(async (r) => {
+    if (r.status === 200) {
+      onSuccess(await r.json())
+    } else {
+      onError(await r.json())
+    }
+  })
